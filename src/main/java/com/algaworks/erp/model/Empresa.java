c9a1096d9b3b4887a81ee15b23b32f128f3ac554
Matheus Vidal <matheusvidal140@gmail.com>
2024-03-29 15:42:29 -0300

    Conversão e validação

--- src/main/java/com/algaworks/erp/model/Empresa.java
+++ src/main/java/com/algaworks/erp/model/Empresa.java
@@ -1,6 +1,7 @@
 package com.algaworks.erp.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -47,6 +48,17 @@ public class Empresa implements Serializable {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, length = 30)
 	private TipoEmpresa tipo;
+	
+	@Column(precision = 10, scale = 2)
+	private BigDecimal faturamento;
+	
+	public BigDecimal getFaturamento() {
+		return faturamento;
+	}
+
+	public void setFaturamento(BigDecimal faturamento) {
+		this.faturamento = faturamento;
+	}
 
 	public Long getId() {
 		return id;

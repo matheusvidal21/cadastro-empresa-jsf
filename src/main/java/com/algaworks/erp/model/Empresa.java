758c7cd965e2ac61354112236ab4217000dfff97
Matheus Vidal <matheusvidal140@gmail.com>
2024-03-30 15:05:02 -0300

    Criando formulário para cadastro

--- src/main/java/com/algaworks/erp/model/Empresa.java
+++ src/main/java/com/algaworks/erp/model/Empresa.java
@@ -49,17 +49,6 @@ public class Empresa implements Serializable {
 	@Column(nullable = false, length = 30)
 	private TipoEmpresa tipo;
 	
-	@Column(precision = 10, scale = 2)
-	private BigDecimal faturamento;
-	
-	public BigDecimal getFaturamento() {
-		return faturamento;
-	}
-
-	public void setFaturamento(BigDecimal faturamento) {
-		this.faturamento = faturamento;
-	}
-
 	public Long getId() {
 		return id;
 	}

f1b89429971a66c1032b9a55b434b6af153fcf99
Matheus Vidal <matheusvidal140@gmail>
2024-03-31 13:08:27 -0300

    Selecionando linhas da tabela de dados

--- src/main/java/controller/GestaoEmpresasBean.java
+++ src/main/java/controller/GestaoEmpresasBean.java
@@ -112,4 +112,12 @@ public class GestaoEmpresasBean implements Serializable {
 	public Empresa getEmpresa() {
 		return empresa;
 	}
+	
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+	public boolean isEmpresaSelecionada() {
+		return empresa != null && empresa.getId() != null;
+	}
 }
